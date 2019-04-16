eb839af3466ea2903d88846d868299d834385ef9
mmekut-mfon.edet <mmekut-mfon.edet@interswitchgroup.com>
2019-04-16 09:51:41 +0100

    manage history added

---
+++ src/component/history/ManageHistory.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+export  class ManageHistory extends Component {
+  render() {
+    return (
+      <div class="btn-group">
+        <button type="button" class="btn btn-primary">
+          Apple
+        </button>
+        <button type="button" class="btn btn-primary">
+          Samsung
+        </button>
+        <button type="button" class="btn btn-primary">
+          Sony
+        </button>
+      </div>
+    );
+  }
+}
+export default ManageHistory;
